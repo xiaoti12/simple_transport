d3d4da1f253c95e72b762836b65f06f6448b4c78
xiaoti <xiaoti@njsk.com>
2025-08-19 17:56:16 +0800

    fix local dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { webdavProxy } from './vite-webdav-proxy'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), webdavProxy()],
   resolve: {
     alias: {
       '@': new URL('./src', import.meta.url).pathname
